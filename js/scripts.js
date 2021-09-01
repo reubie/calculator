82a669309c79a995415d839936ef520d78163f76
reubie <r.nguyo27@gmail.com>
2021-09-01 22:58:21 +0300

    addition of arithmetic functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,34 @@
+// creation of additional function
 var add = function(number1, number2) {
     return number1 + number2;
   };
   var number1 = parseInt(prompt("Enter a number"));
   var number2 = parseInt(prompt("Enter another number"));
   alert(add(number1, number2));
+  
+  // creating subtraction function
+
+  var subtract = function(number1, number2) {
+    return number1 - number2;
+  };
+  var number1 = parseInt(prompt("Enter a number"));
+  var number2 = parseInt(prompt("Enter another number"));
+  alert(subtract(number1, number2));
+
+  // creating a division function
+
+  var divide = function(number1, number2) {
+    return number1 / number2;
+  };
+  var number1 = parseInt(prompt("Enter a number"));
+  var number2 = parseInt(prompt("Enter another number"));
+  alert(divide(number1, number2));
+
+  // creating multiplication function
+
+  var multiply = function(number1, number2) {
+    return number1 * number2;
+  };
+  var number1 = parseInt(prompt("Enter a number"));
+  var number2 = parseInt(prompt("Enter another number"));
+  alert(multiply(number1, number2));

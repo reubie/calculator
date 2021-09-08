8a3d50a6a4c28bebcb3288d7ffd374881d45ce58
reubie <r.nguyo27@gmail.com>
2021-09-08 14:10:51 +0300

    removing the popup and display result on the html page

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,21 @@
-// creation of additional function
 var add = function(number1, number2) {
-    return number1 + number2;
+  return number1 + number2;
   };
-  var number1 = parseInt(prompt("Enter a number"));
-  var number2 = parseInt(prompt("Enter another number"));
-  alert(add(number1, number2));
-  
-  // creating subtraction function
-
   var subtract = function(number1, number2) {
-    return number1 - number2;
+  return number1 - number2;
   };
-  var number1 = parseInt(prompt("Enter a number"));
-  var number2 = parseInt(prompt("Enter another number"));
-  alert(subtract(number1, number2));
-
-  // creating a division function
-
-  var divide = function(number1, number2) {
-    return number1 / number2;
-  };
-  var number1 = parseInt(prompt("Enter a number"));
-  var number2 = parseInt(prompt("Enter another number"));
-  alert(divide(number1, number2));
-
-  // creating multiplication function
-
   var multiply = function(number1, number2) {
-    return number1 * number2;
+  return number1 * number2;
   };
-  var number1 = parseInt(prompt("Enter a number"));
-  var number2 = parseInt(prompt("Enter another number"));
-  alert(multiply(number1, number2));
+  var divide = function(number1, number2) {
+  return number1 / number2;
+  };
+  
+  $(document).ready(function() {
+  $("form#add").submit(function(event) {
+  event.preventDefault();
+  var number1 = parseInt($("#add1").val());
+  var number2 = parseInt($("#add2").val());
+  alert(add(number1, number2));
+  });
+  });

1a6a7e0e1e1b716f8ec370bc343a558608ce35c8
reubie <r.nguyo27@gmail.com>
2021-09-08 14:14:33 +0300

    adding quotes to the js file for easy readability

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,25 @@
+// Business (or back-end) logic:
 var add = function(number1, number2) {
   return number1 + number2;
-  };
-  var subtract = function(number1, number2) {
+};
+var subtract = function(number1, number2) {
   return number1 - number2;
-  };
-  var multiply = function(number1, number2) {
+};
+var multiply = function(number1, number2) {
   return number1 * number2;
-  };
-  var divide = function(number1, number2) {
+};
+var divide = function(number1, number2) {
   return number1 / number2;
-  };
-  
-  $(document).ready(function() {
+};
+
+// User interface (or front-end) logic:
+$(document).ready(function() {
   $("form#add").submit(function(event) {
-  event.preventDefault();
-  var number1 = parseInt($("#add1").val());
-  var number2 = parseInt($("#add2").val());
-  var result = add(number1, number2);
-  $("#output").text(result);
+    event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    var result = add(number1, number2);
+    $("#output").text(result);
   });
-  });
+});
   
